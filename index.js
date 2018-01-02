9b14989d5c830b9df80c802b6e9c442841aa47b0
Patrick Conrey <patrickconrey@gmail.com>
2018-01-02 14:25:01 -0600

    made the server look less stupid lol

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ app.get('/', function(req, res){
 app.get('/css/main.css', function(req, res){
     res.sendFile('/Users/patrickconrey/Documents/websites/resume/css/main.css');
 });
-app.get('assets/me-home-2.jpg', function(req, res){
-    res.sendFile('/Users/patrickconrey/Documents/websites/resume/assets/me-home-2.jpg');
-});
-
 http.listen(3000, function(){
     console.log('listening on *:3000');
 });
